2872a83a07b41a5d52c224421f1820e1172215b7
Hitesh Goel <hiteshgoel426@gmail.com>
2022-06-29 23:32:37 +0530

    changes

--- src/screens/mainApp/App.tsx
+++ src/screens/mainApp/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome People!!!!
+          Welcome to my web!!!!
           <br />
           Site Under Process!!!
         </p>
